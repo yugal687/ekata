a40adc834a0a5f495d7955411ffbb166d27add31
diwashv1 <diwashv1@gmail.com>
2020-09-06 23:59:18 +0545

    order/orderItem/payment- skipped updated CartItemComponent added

---
+++ resources/js/components/Website/cartItemsComponent.vue
@@ -0,0 +1,112 @@
+<template>
+    <div class="">
+        <div class="modal right fade" id="cartModal" tabindex="-1" role="dialog" aria-labelledby="cartModal">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="shopping-cart">
+                        <div class="shopping-cart-header">
+                            <i class="fa fa-shopping-cart cart-icon"></i><span class="badge">
+                            {{$store.state.cartCount}}
+
+                        </span>
+                            <button type="button" class="close text-danger" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <!--end shopping-cart-header -->
+
+                        <ul class="shopping-cart-items">
+                            <li v-if="$store.state.storedLocalStorageProduct" class="clearfix"
+                                v-for="product in $store.state.storedLocalStorageProduct">
+                                <div class="row pb-2 align-items-center">
+                                    <div class="col-4" style="overflow: hidden">
+                                        <img :src="product.image"
+                                             class="img-fluid" width="80px" height="100px"
+                                             alt=""/>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="row">
+                                            <span class="item-name">{{product.product_name}}</span>
+                                        </div>
+                                        <div class="row pb-1">
+                                            <span class="item-price font-weight-bold">{{product.price}}</span>
+                                        </div>
+                                        <!--<div class="row">
+                                            <span class="item-quantity">Quantity:</span>
+                                        </div>-->
+
+                                        <div class="row pr-4">
+                                            <div class="col-xs-3 col-xs-offset-3">
+                                                <div class="input-group input-group-sm number-spinner">
+				                                <span class="input-group-prepend">
+					                                    <button class="btn bg-main-secondary text-white" data-dir="dwn">
+                                                            <i class="fas fa-minus"></i>
+                                                        </button>
+                                                </span>
+                                                    <input type="text" class="form-control text-center" value="1">
+                                                    <span class="input-group-append">
+                                                    <button class="btn bg-main-secondary text-white" data-dir="up">
+                                                        <i class="fas fa-plus"></i>
+                                                    </button>
+				                                </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-2">
+                                        <button @click="remove()"
+                                                class="btn btn-sm btn-danger">
+                                            <i class="fas fa-trash"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </li>
+                            <span v-else>
+                                Your Cart is empty //make nice UX please front devs ;
+                           </span>
+
+                        </ul>
+                        <div class="row">
+                            <div class="col-12 text-right">
+                                <span class="lighter-text">Total:</span>
+                                <span class="main-color-text font-weight-bold">{{$store.state.totalPrice}}</span>
+                            </div>
+                        </div> <!--end shopping-cart-header -->
+
+
+                        <a href="/billings" class="button">Checkout</a>
+                    </div> <!--end shopping-cart -->
+                </div> <!--end container -->
+            </div><!-- modal-dialog -->
+        </div><!-- modal -->
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "cartItemsComponent",
+        //props: ['cartItemCount'],
+        data() {
+            return {
+                product: [],
+                cartItemCount: 0,
+            }
+        },
+
+        methods: {
+            countCartItem() {
+                this.getLocalStorageItem().length;
+            },
+        },
+        computed: {
+            cartItemsCount() {
+                return this.$store.getters.cartItemCount();
+            },
+        },
+
+    }
+</script>
+
+<style scoped>
+
+</style>
